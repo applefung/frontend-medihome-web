5478b6f2aa8b5b6eaba7f614675373fcf0f0d729
amber.fung <amber.fung@FD2019MLT024-0705.local>
2022-01-19 18:12:17 +0800

    first commit

---
+++ lint-staged.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    '*.ts?(x)': () => 'tsc --project tsconfig.json --noEmit',
+    '*.(ts|js)?(x)': ['prettier --write', 'eslint'],
+    // '*.{scss,css}': ['prettier --write'],
+  };
