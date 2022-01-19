a76f010fedac94ab07db3d509bbeca3fce27961b
amber.fung <amber.fung@FD2019MLT024-0705.local>
2022-01-19 18:18:44 +0800

    add text field

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,13 @@
 import type { NextPage } from "next";
+import TextField from "@mui/material/TextField";
 
 const Home: NextPage = () => {
-  return <div>Home Page</div>;
+  return (
+    <div>
+      Home Page
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+    </div>
+  );
 };
 
 export default Home;
