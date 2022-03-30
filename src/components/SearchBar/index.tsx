3a1dd61995531eff3d816f8c4bb2c892e41436db
applefung <804111435@qq.com>
2022-03-30 23:39:48 +0800

    ADD: add new search bar

--- src/components/SearchBar/index.tsx
+++ src/components/SearchBar/index.tsx
@@ -1,5 +1,5 @@
-import { ChangeEvent, useRef } from 'react';
-import TextField from '@mui/material/TextField';
+import { ChangeEvent, KeyboardEvent, useRef } from 'react';
+import { FormControl, InputBase } from '@mui/material';
 import Image from 'next/image';
 import { imageLoader } from '@src/utils/imageLoader';
 import { LOGO } from '@src/utils/constants/images';
@@ -10,20 +10,30 @@ import styles from './styles.module.scss';
 const SearchBar = ({ onSearch }: Record<'onSearch', (value: string) => void>) => {
   const value = useRef('');
 
+  const onKeyPress = ({ key }: KeyboardEvent<HTMLInputElement>) => {
+    if (key === 'Enter') {
+      onSearch(value.current);
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.innerContainer}>
         <Image className={styles.logo} loader={imageLoader} src={LOGO.logoUrl} alt={LOGO.logoAlt} width="250" height="120" />
-        <TextField
-          placeholder="input search text"
-          className={styles.searchBar}
-          onChange={(event: ChangeEvent<HTMLInputElement>) => {
-            value.current = event.target.value;
-          }}
-        />
-        <IconButton type="submit" onClick={() => onSearch(value.current)}>
-          <SearchIcon />
-        </IconButton>
+        <FormControl className={styles.searchBar}>
+          <InputBase
+            autoFocus
+            placeholder="input search text"
+            inputProps={{ 'aria-label': 'search doctors' }}
+            onChange={(event: ChangeEvent<HTMLInputElement>) => {
+              value.current = event.target.value;
+            }}
+            onKeyDown={onKeyPress}
+          />
+          <IconButton type="submit" onClick={() => onSearch(value.current)}>
+            <SearchIcon />
+          </IconButton>
+        </FormControl>
       </div>
     </div>
   );
