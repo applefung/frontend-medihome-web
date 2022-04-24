01b27d6526acce0dfcd5ec5f0338a088dfd3d01f
applefung <804111435@qq.com>
2022-04-24 16:37:27 +0800

    DEL: remove unused code

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -2,7 +2,6 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers, Store } from 'redux';
 import { persistStore, persistReducer, FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
 import authReducer from './authSlice';
 
 const persistConfig = {
